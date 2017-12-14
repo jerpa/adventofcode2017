579c4e5a57e9c3ccf764a939d9c4defb315e63b5
Stefan Samuelsson <stefan@jerpa.net>
2017-12-15 00:27:45 +0100

    day09-2

---
+++ day09-2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		value := scanner.Text()
+		tot := 0
+		val := 0
+		garbage := 0
+		ignore := false
+		for pos := 0; pos < len(value); pos++ {
+			chr := value[pos : pos+1]
+			if chr == "!" {
+				pos++
+			} else if chr == "{" && ignore == false {
+				val++
+			} else if chr == "}" && ignore == false {
+				tot += val
+				val--
+			} else if chr == ">" {
+				ignore = false
+			} else if ignore {
+				garbage++
+			} else if chr == "<" {
+				ignore = true
+			}
+		}
+
+		fmt.Println(garbage)
+	}
+}
